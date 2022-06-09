3b30caac856a726106954610cafd72a92b2d007e
Olga Nadongo <winnienadongo@gmail.com>
2022-06-09 23:26:23 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function(drivers){
+    const newDrivers= drivers.slice(0,2)
+    return newDrivers;
+}
+
+const returnLastTwoDrivers= function(drivers) {
+    return drivers.slice(-2)
+}
+  const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier= function(multiplierFactor){
+   
+    return function(fareFactor){
+        return fareFactor*multiplierFactor
+    }
+}
+ const fareDoubler=createFareMultiplier(2)
+ const fareTripler=createFareMultiplier(3)
+
+
+ const selectDifferentDrivers= function (drivers, returnedDrivers){
+    return returnedDrivers(drivers)
+ }
+ 
