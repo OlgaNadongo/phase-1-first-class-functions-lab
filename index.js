8454b57fac3912fd37341cdcb7d433de13a66f06
Olga Nadongo <winnienadongo@gmail.com>
2022-06-15 05:41:32 +0300

    commit tree 32f9c5748c6acd2b16c1e8c090cc7cb90299132f

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers= function(drivers){
-    const newDrivers= drivers.slice(0,2)
-    return newDrivers;
-}
+// const returnFirstTwoDrivers= function(drivers){
+//     const newDrivers= drivers.slice(0,2)
+//     return newDrivers;
+// }
 
-const returnLastTwoDrivers= function(drivers) {
-    return drivers.slice(-2)
-}
-  const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers]
+// const returnLastTwoDrivers= function(drivers) {
+//     return drivers.slice(-2)
+// }
+//   const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers]
 
-const createFareMultiplier= function(multiplierFactor){
+// const createFareMultiplier= function(multiplierFactor){
    
-    return function(fareFactor){
-        return fareFactor*multiplierFactor
-    }
-}
- const fareDoubler=createFareMultiplier(2)
- const fareTripler=createFareMultiplier(3)
+//     return function(fareFactor){
+//         return fareFactor*multiplierFactor
+//     }
+// }
+//  const fareDoubler=createFareMultiplier(2)
+//  const fareTripler=createFareMultiplier(3)
 
 
- const selectDifferentDrivers= function (drivers, returnedDrivers){
-    return returnedDrivers(drivers)
- }
+//  const selectDifferentDrivers= function (drivers, returnedDrivers){
+//     return returnedDrivers(drivers)
+//  }
  
+const returnFirstTwoDrivers= function (arrayOfDrivers){
+  return arrayOfDrivers.slice(0,2)
+}
+
+const returnLastTwoDrivers= function (arrayOfDrivers){
+    return arrayOfDrivers.slice(Math.max(arrayOfDrivers.length -2,0))
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+//*** function createFareMultiplier(integer){
+//  return newFare 
+// }
+
+// const newFare= function fareAmount(fare){
+//    return fare**integer}
+
+const fareDoubler=createFareMultiplier(2)
